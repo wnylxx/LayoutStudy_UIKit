0b445bbb6d8e44d817601dfe5a2f15c720b0748c
WonyeolHEO <hyy96hyy@naver.com>
2024-07-11 14:22:33 +0900

    initial setting

---
+++ UIKit_Toss/UIKit_Toss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Toss
+//
+//  Created by wonyoul heo on 7/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
