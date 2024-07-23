d61d180e4cfaa7318871e5b1bff85a9372ce396f
WonyeolHEO <hyy96hyy@naver.com>
2024-07-23 21:01:07 +0900

    Create Photo project

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by wonyoul heo on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
