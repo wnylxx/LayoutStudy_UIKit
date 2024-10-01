88f81c080e365f06235e3705d1654e3eef1a3175
WonyeolHEO <hyy96hyy@naver.com>
2024-10-01 23:41:09 +0900

    create and register customCell

--- PhotoAlbum_CollectionView_UIkit/PhotoAlbum_CollectionView_UIkit/CustomCollectionViewCell.swift
+++ PhotoAlbum_CollectionView_UIkit/PhotoAlbum_CollectionView_UIkit/CustomCollectionViewCell.swift
@@ -9,4 +9,39 @@ import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "CustomCollectionViewCell"
+    
+    private let myImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(systemName: "questionmark")
+        iv.tintColor = .white
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    public func configure(with image: UIImage) {
+        self.myImageView.image = image
+        self.setupUI()
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = .systemRed
+        
+        self.addSubview(myImageView)
+        myImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            myImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            myImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            myImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            myImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+            
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.myImageView.image = nil
+    }
 }
--- PhotoAlbum_CollectionView_UIkit/PhotoAlbum_CollectionView_UIkit/ViewController.swift
+++ PhotoAlbum_CollectionView_UIkit/PhotoAlbum_CollectionView_UIkit/ViewController.swift
@@ -20,13 +20,13 @@ class ViewController: UIViewController {
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .systemBackground
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
         return collectionView
     }()
     
     
     
     //MARK: - Lifecycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
@@ -38,6 +38,10 @@ class ViewController: UIViewController {
             images.append(UIImage(named: "PhotoAlbum04")!)
             images.append(UIImage(named: "PhotoAlbum05")!)
         }
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        
     }
 
     private func setupUI() {
@@ -57,4 +61,25 @@ class ViewController: UIViewController {
     }
 
 }
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as? CustomCollectionViewCell else {
+            fatalError("Failed to dequeue CustomCollectionViewCell in HomeControl")
+        }
+        
+        let image = self.images[indexPath.row]
+        cell.configure(with: image)
+        
+        return cell
+    }
+    
+}
+
 
